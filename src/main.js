a3f1e4e75d82612f8df234fa848f4f5b71d9c852
rohto <rohto111117@gmail.com>
2020-04-29 11:19:24 +0900

    dev 動作確認済 firebase 動作確認中 >> 表示のみ確認、index.htmlにvueが適用されていない （router制御の設定がうまくいっていないのではないか状態）

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyACHsvPhxmfWzpVNthNaP6d3CxV0q-nv9g",
+  authDomain: "flowchart-vue.firebaseapp.com",
+  databaseURL: "https://flowchart-vue.firebaseio.com",
+  projectId: "flowchart-vue",
+  storageBucket: "flowchart-vue.appspot.com",
+  messagingSenderId: "578092443616",
+  appId: "1:578092443616:web:20ed517485b6e89e022d85"
+};
+firebase.initializeApp(firebaseConfig);
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
